ab1485b0c2d9adccd5c7f07b47803b422ae49811
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-24 21:33:39 +0100

    Aggiunta header

---
+++ vue-dischi/src/components/myHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid">
+
+    <div class="navbar my_navbar">
+
+        <a class="navbar-brand my_style-brand" href="#">
+            <img :src="img" alt="">
+        </a>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'myHeader',
+    data(){
+        return{
+            img: require("../assets/img/Spotify_App_Logo.svg.png")
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/general.scss";
+.my_navbar{
+    background-color: #000;
+    height: 60px;
+}
+
+img {
+    width: 50px;
+    height: 50px;
+    margin-left: 20px;
+}
+</style>
